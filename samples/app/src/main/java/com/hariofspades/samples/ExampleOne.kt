cd55ea432621d67ee2d9ccd59ab76f44229f3eb9
Hari <hariutd@gmail.com>
2018-09-06 11:36:36 +0530

    implementing example 1 :sparkle:

---
+++ samples/app/src/main/java/com/hariofspades/samples/ExampleOne.kt
@@ -0,0 +1,12 @@
+package com.hariofspades.samples
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ExampleOne : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_one)
+    }
+}
--- samples/app/src/main/java/com/hariofspades/samples/MainActivity.kt
+++ samples/app/src/main/java/com/hariofspades/samples/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.hariofspades.samples
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.reflect.GenericDeclaration
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        clickListeners()
     }
+
+    private fun clickListeners() {
+
+        simple.setOnClickListener {
+            startActivity(Intent(this, ExampleOne::class.java))
+        }
+    }
+
+
 }
